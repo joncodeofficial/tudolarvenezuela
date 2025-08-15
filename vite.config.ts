a20bb6596bd17b965a3e9afaaf91621955e245fa
jonpena <gran_200014@hotmail.com>
2025-08-16 00:39:07 +0200

    chore: config tailwind v4

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [tailwindcss(), react()],
   base: './',
 });
